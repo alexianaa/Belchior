3fef05150c5fb4587684981537231712699f51cd
vitorlvr <vsouza954@gmail.com>
2021-10-16 21:41:19 -0300

    banco de dados de avaliações

--- avaliacoes.sql
+++ avaliacoes.sql
@@ -1,57 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 06-Out-2021 às 21:17
--- Versão do servidor: 10.4.21-MariaDB
--- versão do PHP: 8.0.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `avaliacao_lojas`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `avaliacoes`
---
 
 CREATE TABLE `avaliacoes` (
   `id` int(11) NOT NULL,
   `qtd_estrela` int(11) NOT NULL,
-  `created` datetime NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modified` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `avaliacoes`
---
 ALTER TABLE `avaliacoes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`modified`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
 
---
--- AUTO_INCREMENT de tabela `avaliacoes`
---
 ALTER TABLE `avaliacoes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `modified` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- html/avaliacao.php
+++ html/avaliacao.php
@@ -3,4 +3,18 @@
     $id = $_POST['id'];
     $output = $id ." ". $nota;
     echo $output;
+
+    //conexao com o banco de dados
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $bd   = 'avaliacao_lojas';
+
+    $conectar = mysqli_connect( $host, $user, $pass, $bd);
+
+    //enviando avaliacoes para o banco de dados
+
+    $result_usuario = "INSERT INTO avaliacoes (id, qtd_estrela)
+    VALUES ('$id', '$nota')";
+    $resultado_usuario = mysqli_query($conectar, $result_usuario);
 ?>
